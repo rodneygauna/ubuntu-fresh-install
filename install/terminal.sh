8c309295ab823bf7adefd1fd6de6d492c3e3ee67
Rodney Gauna <rodneygauna@gmail.com>
2025-12-09 11:21:43 -0800

    Consolidate library installations into terminal.sh and remove redundant libraries.sh

--- install/terminal.sh
+++ install/terminal.sh
@@ -5,5 +5,10 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
+# Install common development libraries needed for language installations
+sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev \
+  libbz2-dev libsqlite3-dev libffi-dev liblzma-dev libncurses5-dev \
+  libxml2-dev libxmlsec1-dev libyaml-dev
+
 # Run terminal installers
 for installer in ~/.local/share/ubuntu-fresh-install/install/terminal/*.sh; do source $installer; done
--- install/terminal/libraries.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install common development libraries
-sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev \
-  libbz2-dev libsqlite3-dev libffi-dev liblzma-dev libncurses5-dev \
-  libxml2-dev libxmlsec1-dev libyaml-dev
